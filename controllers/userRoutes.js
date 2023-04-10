e7408afcd48af4e3bf7324aeb52ada546be101f0
Cmeesh11 <cartermeesh11@gmail.com>
2023-04-10 10:39:53 -0400

    created login route

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,10 +1,28 @@
 const userRouter = require('express').Router();
 
-
-userRouter.post('/login', (req, res) => {
-  
+// Login for existing user
+userRouter.post('/login', async (req, res) => {
+  try {
+  const user = await User.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+  if (await user.checkPassword(req.body.password)) {
+    req.session.save(() => {
+      req.session.logged_in = true;
+      req.session.id = user.id;
+    }) 
+    res.status(200).json({ message: "Successfully logged in!" })
+  }
+} catch {
+  res.status(404).render('login', {
+    notExist: true
+  })
+}
 })
 
+// Brings user to dashboard if they are logged_in
 userRouter.get('/dashboard', (req, res) => {
   res.render('dashboard', {
     logged_in: req.session.logged_in
--- models/User.js
+++ models/User.js
@@ -4,5 +4,38 @@ const sequelize = require('../config/connection');
 
 
 class User extends Model {
-
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    hooks: {
+      async beforeCreate(userInstance) {
+        userInstance.password = await bcrypt.hash(userInstance.password, 10);
+        return userInstance;
+      }
+    },
+    sequelize,
+    modelName: 'user',
+    freezeTableName: true,
+    timestamps: false
+  }
+)
